5a8ce6aa35ba1f85b60ae46c4e7cbf642392a0e7
tauqeer180 <tauqeermalik855@gmail.com>
2023-10-19 15:35:34 +0500

    push

---
+++ assets/js/carousel.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselWrapper = document.querySelector(".carousel-wrapper");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+  const cardWidth = carouselWrapper.clientWidth / 3; // Width of one card
+  console.log("card Width", cardWidth);
+  let currentIndex = 0;
+
+  function updateCarousel() {
+    const translateX = -currentIndex * cardWidth;
+    carouselWrapper.style.transform = `translateX(${translateX}px)`;
+  }
+
+  function showNextCard() {
+    if (currentIndex < carouselWrapper.children.length - 3) {
+      currentIndex++;
+      updateCarousel();
+    }
+  }
+
+  function showPrevCard() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateCarousel();
+    }
+  }
+
+  nextBtn.addEventListener("click", showNextCard);
+  prevBtn.addEventListener("click", showPrevCard);
+});
